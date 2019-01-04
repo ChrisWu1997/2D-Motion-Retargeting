ec7625bde249dd62512671a5c4441bda18746f75
Rundi Wu <rundi_wu@pku.edu.cn>
2019-01-04 21:41:40 +0800

    move motion2video into functional and modify joint2image to be compatible different number of joints

--- functional/visualization.py
+++ functional/visualization.py
@@ -4,6 +4,7 @@ import cv2
 import math
 import imageio
 from tqdm import tqdm
+from PIL import Image
 
 
 def interpolate_color(color1, color2, alpha):
@@ -43,23 +44,40 @@ def hex2rgb(hex, number_of_colors=3):
     return rgb
 
 
-def joints2image(joints_position, colors, transparency=False, H=512, W=512, imtype=np.uint8):
+def joints2image(joints_position, colors, transparency=False, H=512, W=512, nr_joints=49, imtype=np.uint8):
 
-    limbSeq = [[0, 1], [1, 2], [1, 5], [1, 8], [2, 3], [3, 4], [5, 6], [6, 7], \
-               [8, 9], [8, 13], [9, 10], [10, 11], [11, 12], [13, 14], [14, 15], [15, 16],
-               [0, 17], [0, 18]]
+    if nr_joints == 49: # full joints(49): basic(15) + eyes(2) + toes(2) + hands(30)
+        limbSeq = [[0, 1], [1, 2], [1, 5], [1, 8], [2, 3], [3, 4], [5, 6], [6, 7], \
+                   [8, 9], [8, 13], [9, 10], [10, 11], [11, 12], [13, 14], [14, 15], [15, 16],
+                   ]#[0, 17], [0, 18]] #ignore eyes
 
-    L = rgb2rgba(colors[0]) if transparency else colors[0]
-    M = rgb2rgba(colors[1]) if transparency else colors[1]
-    R = rgb2rgba(colors[2]) if transparency else colors[2]
+        L = rgb2rgba(colors[0]) if transparency else colors[0]
+        M = rgb2rgba(colors[1]) if transparency else colors[1]
+        R = rgb2rgba(colors[2]) if transparency else colors[2]
 
-    colors_joints = [M, M, L, L, L, R, R, \
-              R, M, L, L, L, L, R, R, R,
-              R, R, L] + [L] * 15 + [R] * 15
+        colors_joints = [M, M, L, L, L, R, R,
+                  R, M, L, L, L, L, R, R, R,
+                  R, R, L] + [L] * 15 + [R] * 15
 
-    colors_limbs = [M, L, R, M, L, L, R, \
-              R, L, R, L, L, L, R, R, R, \
-              R, R]
+        colors_limbs = [M, L, R, M, L, L, R,
+                  R, L, R, L, L, L, R, R, R,
+                  R, R]
+    elif nr_joints == 15 or nr_joints == 17: # basic joints(15) + (eyes(2))
+        limbSeq = [[0, 1], [1, 2], [1, 5], [1, 8], [2, 3], [3, 4], [5, 6], [6, 7],
+                   [8, 9], [8, 12], [9, 10], [10, 11], [12, 13], [13, 14]]
+                    # [0, 15], [0, 16] two eyes are not drawn
+
+        L = rgb2rgba(colors[0]) if transparency else colors[0]
+        M = rgb2rgba(colors[1]) if transparency else colors[1]
+        R = rgb2rgba(colors[2]) if transparency else colors[2]
+
+        colors_joints = [M, M, L, L, L, R, R,
+                         R, M, L, L, L, R, R, R]
+
+        colors_limbs = [M, L, R, M, L, L, R,
+                        R, L, R, L, L, R, R]
+    else:
+        raise ValueError("Only support number of joints be 49 or 17 or 15")
 
     if transparency:
         canvas = np.zeros(shape=(H, W, 4))
@@ -76,7 +94,7 @@ def joints2image(joints_position, colors, transparency=False, H=512, W=512, imty
 
     cv2.circle(canvas, (int(joints_position[0][0]),int(joints_position[0][1])), head_radius, colors_joints[0], thickness=-1)
 
-    for i in range(1,49):
+    for i in range(1, len(colors_joints)):
         if i in (17, 18):
             continue
         elif i > 18:
@@ -87,7 +105,7 @@ def joints2image(joints_position, colors, transparency=False, H=512, W=512, imty
 
     stickwidth = 2
 
-    for i in range(16):
+    for i in range(len(limbSeq)):
         limb = limbSeq[i]
         cur_canvas = canvas.copy()
         point1_index = limb[0]
@@ -110,6 +128,26 @@ def joints2image(joints_position, colors, transparency=False, H=512, W=512, imty
         canvas_cropped = canvas[:,bb[2]:bb[3], :]
 
     return [canvas.astype(imtype), canvas_cropped.astype(imtype)]
+
+
+def motion2video(motion, h, w, save_path, colors, transparency=False, motion_tgt=None, fps=25, nr_joints=49):
+    videowriter = imageio.get_writer(os.path.join(save_path, 'video.mp4'), fps=fps)
+    vlen = motion.shape[-1]
+    frames_dir = os.path.join(save_path)
+    #ensure_dir(frames_dir)
+    for i in tqdm(range(vlen)):
+        [img, img_cropped] = joints2image(motion[:, :, i], colors, transparency, H=h, W=w, nr_joints=nr_joints)
+        if motion_tgt is not None:
+            [img_tgt, img_tgt_cropped] = joints2image(motion_tgt[:, :, i], colors, H=h, W=w, nr_joints=nr_joints)
+            img = cv2.addWeighted(img_tgt, 0.3, img, 0.7, 0)
+        save_image(img_cropped, os.path.join(frames_dir, "%04d.png" % i))
+        videowriter.append_data(img)
+    videowriter.close()
+
+
+def save_image(image_numpy, image_path):
+    image_pil = Image.fromarray(image_numpy)
+    image_pil.save(image_path)
 
 
 def bounding_box(img):
